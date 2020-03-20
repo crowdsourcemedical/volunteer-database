a69f1d2ea161dbc0f9866b0e079bc0d07ffcfcc9
Stan Hettinga <stan@codeyellow.nl>
2020-03-20 21:40:41 +0100

    add frontend

---
+++ frontend/src/components/header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export default class Header extends Component{
+
+	render(){
+		return (
+			<ul>
+				<li><a href='#'>Home</a></li>
+				<li><a href='#'>Signin</a></li>
+			</ul>
+		)
+	}
+}
---
+++ frontend/src/pages/home.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class HomePage extends Component {
+
+	render(){
+		return (
+			<h1>Crowd sourced medical</h1>
+		)
+	}
+}
+
