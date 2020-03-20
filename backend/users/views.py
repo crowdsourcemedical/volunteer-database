b0f03d29cb3aa0442e342b57c5a35dcbefeb69ad
Stan Hettinga <stan@codeyellow.nl>
2020-03-20 21:16:55 +0100

    Fixed errors in pull requests

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,28 +1,31 @@
 from rest_framework import mixins, generics, permissions
-from django.contrib.auth.models import User
-from myapp.serializers import UserSerializer
+from django.contrib.auth import get_user_model 
+from .serializers import UserSerializer
 
-
+Users = get_user_model()
 class getUsers(generics.ListAPIView):
     """
     List all users, should be able to filter by skill with a query param
     """
-    queryset = Users.list.all()
+
+    queryset = Users.objects.all()
     serial = UserSerializer
     
-    def listAll(self, skill='none', request):
+    def listAll(self, request):
         """
         returns a list of all users, if there is a skill defined then only users with that skill will be returned
         """
+        # Skill should be retrieved from query string
+        skill = None
         queryset = self.get_queryset()
         serial = UserSerializer(queryset)
         userList = Response(serial.data) #Gets a list of User objects from the serializer
-        if skill = 'none':  #Checks the optional skill filter
+        if skill == 'none':  #Checks the optional skill filter
             return userList
         else:
             output = []
             for user in UserList: #Loops through all users to find the ones with the skill requested for and only returns thouse
-                if user.skill = skill:
+                if user.skill == skill:
                     output.append(user)
             return output
     
