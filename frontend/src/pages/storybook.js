6871e15b341dcf9ed2605f9fa155c007ab6a6e92
Stan Hettinga <stan@codeyellow.nl>
2020-03-22 15:15:03 +0100

    Update user card to accept a user object

--- frontend/src/pages/storybook.js
+++ frontend/src/pages/storybook.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import UserSmallCard from '../components/Cards/userSmallCard.js';
 
+
+const mockUser = {
+	id: '1',
+	name: 'derjanger',
+	description: 'Experienced 3D modeler in CAD. Experience with medical supplise/facemasks. Ready to help.',
+	skills: ['3d Modeling', 'Medical Supplies', 'facemasks', 'more', 'more2', 'more3'],
+	status: 'Looking to collaborate'
+}
+
 const StoryBook = props =>  {
 
 
@@ -8,7 +17,7 @@ const StoryBook = props =>  {
         <React.Fragment>
             <h1>User Small Card</h1>
             <div style={{display: 'flex', justifyContent: 'center'}}>
-                <UserSmallCard />
+                <UserSmallCard user={mockUser} />
             </div>
         
         </React.Fragment>
