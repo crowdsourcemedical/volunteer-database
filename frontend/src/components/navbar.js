2fb55fceb83297043a111d5f01c301b19b526a01
stephanLeece <stephan.leece@outlook.com>
2020-03-22 20:28:11 +0100

    split out auth links from page links, move links into drawer component

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,41 +1,154 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Link from '@material-ui/core/Link';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { PAGE_LINKS_LIST } from '../constants.js';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Link from '@material-ui/core/Link';
+
+import { PAGE_LINKS_LIST, LOGIN_PAGE_LINK } from '../constants.js';
+
+const drawerWidth = 240;
+
+const ListItemLink = props => <ListItem button component="a" {...props} />;
 
 const useStyles = makeStyles(theme => ({
 	root: {
-		flexGrow: 1,
+		display: 'flex',
+	},
+	appBar: {
+		transition: theme.transitions.create(['margin', 'width'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+	},
+	appBarShift: {
+		width: `calc(100% - ${drawerWidth}px)`,
+		marginLeft: drawerWidth,
+		transition: theme.transitions.create(['margin', 'width'], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	},
+	authLink: {
+		marginLeft: 'auto',
+		color: 'inherit',
 	},
 	menuButton: {
 		marginRight: theme.spacing(2),
 	},
-	title: {
+	hide: {
+		display: 'none',
+	},
+	drawer: {
+		width: drawerWidth,
+		flexShrink: 0,
+	},
+	drawerPaper: {
+		width: drawerWidth,
+	},
+	drawerHeader: {
+		display: 'flex',
+		alignItems: 'center',
+		padding: theme.spacing(0, 1),
+		// necessary for content to be below app bar
+		...theme.mixins.toolbar,
+		justifyContent: 'flex-end',
+	},
+	content: {
 		flexGrow: 1,
+		padding: theme.spacing(3),
+		transition: theme.transitions.create('margin', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+		marginLeft: -drawerWidth,
+	},
+	contentShift: {
+		transition: theme.transitions.create('margin', {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+		marginLeft: 0,
 	},
 }));
 
 const NavBar = () => {
 	const classes = useStyles();
+	const theme = useTheme();
+	const [open, setOpen] = React.useState(false);
+
+	const handleDrawerOpen = () => {
+		setOpen(true);
+	};
+
+	const handleDrawerClose = () => {
+		setOpen(false);
+	};
+
 	return (
 		<div className={classes.root}>
-			<AppBar position="static">
+			<CssBaseline />
+			<AppBar
+				position="fixed"
+				className={clsx(classes.appBar, {
+					[classes.appBarShift]: open,
+				})}
+			>
 				<Toolbar>
-					<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+					<IconButton
+						color="inherit"
+						aria-label="open drawer"
+						onClick={handleDrawerOpen}
+						edge="start"
+						className={clsx(classes.menuButton, open && classes.hide)}
+					>
 						<MenuIcon />
 					</IconButton>
-					{PAGE_LINKS_LIST &&
-						PAGE_LINKS_LIST.map(link => (
-							<Link color="textSecondary" href={link.path}>
-								{link.name}
-							</Link>
-						))}
+					<Typography variant="h6" noWrap>
+						App Title
+					</Typography>
+					<Link href={LOGIN_PAGE_LINK.path} className={classes.authLink}>
+						<Typography variant="h6" noWrap>
+							{LOGIN_PAGE_LINK.name}
+						</Typography>
+					</Link>
 				</Toolbar>
 			</AppBar>
+			<Drawer
+				className={classes.drawer}
+				anchor="left"
+				open={open}
+				classes={{
+					paper: classes.drawerPaper,
+				}}
+			>
+				<div className={classes.drawerHeader}>
+					<IconButton onClick={handleDrawerClose}>
+						{theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+					</IconButton>
+				</div>
+				<Divider />
+				<List>
+					{PAGE_LINKS_LIST &&
+						PAGE_LINKS_LIST.map(link => (
+							<ListItemLink href={link.path}>
+								<ListItemText primary={link.name} />
+							</ListItemLink>
+						))}
+				</List>
+				<Divider />
+			</Drawer>
 		</div>
 	);
 };
