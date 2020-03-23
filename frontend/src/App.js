6db4a5268a83558e81a90fd9b9d32e513f0e0a4d
dumblole <dumblole@gmailcom>
2020-03-22 21:23:11 -0400

    about css and hero section p much done aside from some tweaking

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import Header from './components/header.js'
-import HomePage from './pages/home.js'
-import SearchPage from './pages/search.js'
-import AboutPage from './pages/about.js'
-import LoginPage from './pages/login.js'
-import SignupPage from './pages/signup.js'
-import SignupVolunteerPage from './pages/signup_volunteer'
-import SignupProjectPage from './pages/signup_project'
-import StoryBook from './pages/storybook.js'
-import NewProject from './pages/newproject.js'
-import './App.css';
+import Header from "./components/header.js";
+import HomePage from "./pages/home.js";
+import SearchPage from "./pages/search.js";
+import AboutPage from "./pages/about.js";
+import LoginPage from "./pages/login.js";
+import SignupPage from "./pages/signup.js";
+import SignupVolunteerPage from "./pages/signup_volunteer";
+import SignupProjectPage from "./pages/signup_project";
+import StoryBook from "./pages/storybook.js";
+import NewProject from "./pages/newproject.js";
+import "./App.css";
+import "./about.css";
 
 function App() {
   return (
@@ -23,16 +24,17 @@ function App() {
           <Route path="/search" exact component={SearchPage} />
           <Route path="/login" exact component={LoginPage} />
           <Route path="/signup" exact component={SignupPage} />
-          <Route path="/signup/volunteer" exact component={SignupVolunteerPage} />
+          <Route
+            path="/signup/volunteer"
+            exact
+            component={SignupVolunteerPage}
+          />
           <Route path="/signup/project" exact component={SignupProjectPage} />
           <Route path="/storybook" exact component={StoryBook} />
           <Route path="/newproject" exact component={NewProject} />
-          
-
         </Switch>
       </div>
     </Router>
-
   );
 }
 
