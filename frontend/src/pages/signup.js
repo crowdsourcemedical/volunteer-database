629aa4f63f235921bec48918a8a5d9712221c495
Andrew London <london.andrewd@gmail.com>
2020-03-23 09:49:39 -0400

    signup page started

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,4 +1,16 @@
 import React, {Component} from 'react';
+import GooglePlaceAutocomplete from 'material-ui-autocomplete-google-places';
+import {
+	FormControl,
+	InputLabel,
+	Select,
+	MenuItem,
+	Checkbox,
+	FormGroup,
+	TextField,
+	FormControlLabel
+} from '@material-ui/core'
+import { ComputerScienceTypes, MedicalTypes } from '../types/SignupTypes';
 
 export default class SignupPage extends Component {
 	constructor(props){
@@ -8,9 +20,13 @@ export default class SignupPage extends Component {
 			password1: '',
 			password2: '',
 			skill: '',
-			loc: ''
+			loc: '',
+			selectedField: 'medical',
+			selectedSpecialty: '',
+			aboutText: '',
 		}
 		this.handleChange = this.handleChange.bind(this)
+		this.handleButtonClick = this.handleButtonClick.bind(this)
 		this.handleSumbit = this.handleSubmit.bind(this)
 
 	}
@@ -18,39 +34,67 @@ export default class SignupPage extends Component {
 	handleChange(e) {
 		this.setState({[e.target.name]: e.target.value})
 	}
+
+	// Seperate click handler needed for material UI button due to currentTarget object
+	handleButtonClick(e) {
+		this.setState({[e.currentTarget.name]: e.currentTarget.value})
+	}
 	handleSubmit(e) {
 		/*Send a api call to the backend. Redirect to different page */
 	}
 
 	validate(){
 		/* Should check if everything is filled out and passwords match */
-		return 'true' 
+		return 'true'
 	}
+
+	renderSpecialties(text) {
+		return (
+		<FormControlLabel
+			key={text}
+			control={
+				<Checkbox
+					checked={this.state.selectedSpecialty === text}
+					onChange={() => this.setState({ selectedSpecialty: text })}
+					color="primary"
+					name={text} />
+				}
+			label={text}
+		/>
+	)}
 
 
 	render() {
 		return(
-			<form onSubmit={this.handleSubmit}>
-				<label>email</label>
-				<input type='email' name='email' value={this.state.email} onChange={this.handleChange}></input>
-
-				<label>password</label>
-				<input type='password' name='password1' value={this.state.password1} onChange={this.handleChange}></input>
-
-				<label>repeat password</label>
-				<input type='password' name='password2' value={this.state.password2} onChange={this.handleChange}></input>
-
-				<label>Skill</label>
-				<select name='skill'>
-					<option value="a">A</option>
-					<option value="b">B</option>
-				</select>
-
-				<label>Locaction</label>
-				<input type='text' name='loc' value={this.state.loc} onChange={this.handleChange}></input>
-
-				<input type='submit' disabled={this.validate()} value='submit'></input>
-			</form>
+			<div className="signup__container">
+					<InputLabel id="field">I work in the field of...</InputLabel>
+					<Select
+						className="field-select"
+						name="selectedField"
+						value={this.state.selectedField}
+						onChange={this.handleChange}
+					>
+						<MenuItem value={'medical'}>Medical</MenuItem>
+						<MenuItem value={'computerScience'}>Computer Science</MenuItem>
+					</Select>
+	        <FormGroup>
+						{this.state.selectedField === 'computerScience' && ComputerScienceTypes.map((text) => (this.renderSpecialties(text)))}
+	          {this.state.selectedField === 'medical' && MedicalTypes.map((text) => (this.renderSpecialties(text)))}
+					</FormGroup>
+					<GooglePlaceAutocomplete
+					  types={['cities']}
+					/>
+					<TextField
+						className="about-text"
+						label="Tell us about yourself. What is your experience and how much time do you have to contribute?"
+						name="aboutText"
+						multiline
+						variant="filled"
+						rows="8"
+						value={this.state.aboutText}
+						onChange={this.handleChange}
+					/>
+			</div>
 		)
 	}
 }
