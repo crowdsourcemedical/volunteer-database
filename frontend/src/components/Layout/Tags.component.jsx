83e07c0b5cd09956328f30b664f7e14b9aafbae8
Khoi Hoang <hello@koicha.dev>
2020-03-23 01:56:48 +0100

    Added Create User Page
    Almost Complete, but missing something:
    - Will add a state component, so in the future, we need to fetch the data from the API
    - Will Fix image header on the top
    - Need to fix description correctly

---
+++ frontend/src/components/Layout/Tags.component.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Chip from '@material-ui/core/Chip';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    margin: '1em 0px',
+    float: 'right',
+    '& > *': {
+      margin: theme.spacing(0.5)
+    }
+  }
+}));
+
+export default function SmallChips() {
+  const classes = useStyles();
+
+  const handleClick = () => {
+    console.info('You clicked the Chip.');
+  };
+
+  return (
+    <div className={classes.root}>
+      <Chip
+        size='medium'
+        label='Clickable Link'
+        component='a'
+        href='#chip'
+        clickable
+        onClick={handleClick}
+      />
+      <Chip
+        size='medium'
+        label='Clickable Link'
+        component='a'
+        href='#chip'
+        clickable
+        onClick={handleClick}
+      />
+      <Chip
+        size='medium'
+        label='Clickable Link'
+        component='a'
+        href='#chip'
+        clickable
+        onClick={handleClick}
+      />
+      <Chip
+        size='medium'
+        label='Clickable Link'
+        component='a'
+        href='#chip'
+        clickable
+        onClick={handleClick}
+      />
+      <Chip
+        size='medium'
+        label='Clickable Link'
+        component='a'
+        href='#chip'
+        clickable
+        onClick={handleClick}
+      />
+      <Chip
+        size='medium'
+        label='Clickable Link'
+        component='a'
+        href='#chip'
+        clickable
+        onClick={handleClick}
+      />
+      <Chip
+        size='medium'
+        label='Clickable Link'
+        component='a'
+        href='#chip'
+        clickable
+        onClick={handleClick}
+      />
+    </div>
+  );
+}
---
+++ frontend/src/components/Layout/User-profile.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345
+  }
+});
+
+export default function ImgMediaCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component='img'
+          alt='Contemplative Reptile'
+          height='140'
+          image='https://material-ui.com/static/images/cards/contemplative-reptile.jpg'
+          title='Contemplative Reptile'
+        />
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='h2'>
+            Lizard
+          </Typography>
+          <Typography variant='body2' color='textSecondary' component='p'>
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
