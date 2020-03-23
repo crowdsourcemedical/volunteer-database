99df8244ab9139bdac93db9dfcb59fbc70a5b01c
dennispio <dennis.piotrowski@web.de>
2020-03-23 19:34:27 +0100

    about homepage

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,12 +3,25 @@ import PropTypes from "prop-types";
 //import API from "../../api";
 import { Menu, MenuItem, Box, Typography, Grid, Paper } from "@material-ui/core";
 import { IconButton, Button, Fab } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { Hero } from "../components/Hero/Hero";
+import AboutImg from "../images/about.png";
+import Container from "@material-ui/core/Container";
+
 //import ProjectSmallCard from "../ProjectComponents/ProjectSmallCard";
 
 import HeroImg from "../images/hero.png";
+import shadows from "@material-ui/core/styles/shadows";
+
+const useStyles = makeStyles(theme => ({
+  image: {
+		boxShadow: '-9px 10px 0px 1px #006772',
+		width: '100%'
+  },
+}));
 
 const HomePage = props => {
+	const classes = useStyles();
   return (
     <Grid container spacing={2}>
       <Grid container>
@@ -24,6 +37,33 @@ const HomePage = props => {
           </Typography>
         </Hero>
       </Grid>
+			<Container>
+				<Grid container align="left">
+					<Grid item lg={5}>
+						<Typography align="left" variant="h3" >Project Information</Typography>
+						<Box m={2} />
+						<Typography align="left" variant="p">
+								Regulated medical devices are the purview of the Food and Drug Administration. Any designer or
+								manufacturer producing items claiming to prevent, diagnose, treat, or cure a medical condition
+								are under their jurisdiction and subject to premarket approval. Punishments are wide-ranging,
+								but our recommendation is to avoid them altogether.
+						</Typography>
+						<Box m={4} />
+						<Typography align="left" variant="p">
+							Words like "medical", "prevent", "protect", and "hospital" are going to put you at risk.
+							Indicating your mask is for a non-medical purpose is not recommended, because the intent of this
+							effort is clear to authorities. When packaging, distributing, or discussing these produced
+							materials, we recommend using plain boxes wherever possible, including no claims or disclaimers.
+						</Typography>
+					</Grid>
+					<Grid item lg={2}/>
+					<Grid item lg={5}>
+						<div>
+							<img className={classes.image} src={AboutImg} />
+						</div>
+					</Grid>
+				</Grid>
+			</Container>
       {/* Footer */}
       <Grid item xs={12} />
       {/* End Main Page Content */}
