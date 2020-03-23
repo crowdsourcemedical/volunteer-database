31a2fad8c93a78f81b4f9a228dc51e566afc967e
arlinters <charlie.winters17@gmail.com>
2020-03-23 16:24:36 -0500

    Update to include router for user

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,43 +1,14 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, APIRouter
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 from typing import List
 from sqlalchemy.orm import Session
+from .routers import users
 
 models.Base.metadata.create_all(bind=engine)
 CSM_Backend = FastAPI()
 
-
-def get_db():
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
-
-
-@CSM_Backend.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, user_email=user.user_email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email is already registered!")
-    return crud.create_user(db=db, user=user)
-
-
-@CSM_Backend.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-
-@CSM_Backend.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
+CSM_Backend.include_router(users.CSM_router)
 @CSM_Backend.get("/")
 async def root():
     return {"message": "This is the root of the API. Please go to site.com/docs to see the documentation"}
