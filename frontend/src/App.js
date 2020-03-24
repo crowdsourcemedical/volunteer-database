202bc53a536c355e95e886e11cef9cda82899ce0
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2020-03-24 17:18:07 -0400

    Merge Frontend/Staging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import AboutPage from './pages/about.js';
 import SignupPage from './pages/signup.js';
 import SignupVolunteerPage from './pages/signup_volunteer';
 import StoryBook from './pages/storybook.js';
-import NewProject from './pages/newproject.js';
+import ProjectSubmit from './pages/ProjectSubmit.js';
 import Style from './pages/style.js';
 import Project from './pages/project';
 import UserProfile from './components/UserProfile/Profile.component';
@@ -16,26 +16,23 @@ import './App.css';
 import './about.css';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/about" exact component={AboutPage} />
-          <Route path="/search" exact component={SearchPage} />
-          <Route path="/signup" exact component={SignupPage} />
-          <Route path="/signup/volunteer" exact component={SignupVolunteerPage} />
-          <Route path="/storybook" exact component={StoryBook} />
-          <Route path="/newproject" exact component={NewProject} />
-          <Route path="/userprofile" exact component={UserProfile} />
-          <Route path="/profile" exact component={Profile} />
-          <Route path="/style" exact component={Style} />
-          <Route path="/project" exact component={Project} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
+	return (
+		<Router>
+			<div className="App">
+				<NavBar />
+				<Switch>
+					<Route path="/" exact component={HomePage} />
+					<Route path="/about" exact component={AboutPage} />
+					<Route path="/search" exact component={SearchPage} />
+					<Route path="/signup" exact component={SignupPage} />
+					<Route path="/signup/volunteer" exact component={SignupVolunteerPage} />
+					<Route path="/storybook" exact component={StoryBook} />
+					<Route path="/projects/submit" exact component={ProjectSubmit} />
+					<Route path="/style" exact component={Style} />
+ 					<Route path='/userprofile' exact component={UserProfile} />
+				</Switch>
+			</div>
+		</Router>
+	);
+  }
 export default App;
