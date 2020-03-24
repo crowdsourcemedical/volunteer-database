b019a683a3dd8b169af88349bec9132d66cd62b5
dumblole <dumblole@gmailcom>
2020-03-23 20:16:02 -0400

    textbox background changed to match figma, grid is responsive, currently WIP responsive checkbox + margins

--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -1,6 +1,6 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
 // Single source for colors for easy adjustments
 // NOTE: Some colors are not being used but are part of the Figma
@@ -8,29 +8,29 @@ import React, { Fragment } from 'react';
 //    if they are never utilized.
 const colors = {
   // Primary
-  primary: '#006772',
-  primaryLight: '#049BAB',
-  primaryDark: '#049BAB', // This is active tint for primary button animations
+  primary: "#006772",
+  primaryLight: "#049BAB",
+  primaryDark: "#049BAB", // This is active tint for primary button animations
   // Secondary
-  secondary: '#320072',
-  secondaryLight: '#5906C4',
-  secondaryDark: '#5906C4', // This is active tint for secondary button animations
+  secondary: "#320072",
+  secondaryLight: "#5906C4",
+  secondaryDark: "#5906C4", // This is active tint for secondary button animations
   // Tertiary
-  tertiary: '#3E4C5E',
+  tertiary: "#3E4C5E",
   // Grays
-  grayDark: '#8D8D8F',
-  grayMedium: '#DADADA',
-  grayLight: '#F2F5F7',
+  grayDark: "#8D8D8F",
+  grayMedium: "#DADADA",
+  grayLight: "#F2F5F7",
   // Accent/error,
-  accent: '#AB2346',
+  accent: "#AB2346",
   // Text/font
-  textPrimary: '#06070B',
-  textSecondary: '#828282',
+  textPrimary: "#06070B",
+  textSecondary: "#828282",
   // Status/toast
-  error: '#F44336',
-  warning: '#FF9800',
-  info: '#2196F3',
-  success: '#4CAF50',
+  error: "#F44336",
+  warning: "#FF9800",
+  info: "#2196F3",
+  success: "#4CAF50"
 };
 
 const theme = createMuiTheme({
@@ -39,96 +39,99 @@ const theme = createMuiTheme({
     fontFamily: '"Muli", sans-serif',
     h1: {
       fontFamily: '"Poppins", sans-serif',
-      fontSize: '4rem',
-      fontWeight: 'bold',
+      fontSize: "4rem",
+      fontWeight: "bold"
     },
     h2: {
       fontFamily: '"Poppins", sans-serif',
-      fontSize: '2rem',
-      fontWeight: 'bold',
+      fontSize: "2rem",
+      fontWeight: "bold"
     },
     h3: {
       fontFamily: '"Poppins", sans-serif',
-      fontSize: '1.5rem',
-      fontWeight: 'bold',
+      fontSize: "1.5rem",
+      fontWeight: "bold"
     },
     h4: {
       fontFamily: '"Poppins", sans-serif',
-      fontSize: '1.25rem',
-      fontWeight: 'bold',
+      fontSize: "1.25rem",
+      fontWeight: "bold"
     },
     h5: {
       fontFamily: '"Poppins", sans-serif',
-      fontSize: '1rem',
-      fontWeight: 'bold',
+      fontSize: "1rem",
+      fontWeight: "bold"
     },
     h6: {
       fontFamily: '"Muli", sans-serif',
-      fontSize: '1rem',
-      color: colors.textSecondary,
+      fontSize: "1rem",
+      color: colors.textSecondary
     },
     body1: {
-      fontSize: '1rem',
-      color: colors.textPrimary,
+      fontSize: "1rem",
+      color: colors.textPrimary
     },
     caption: {
       fontFamily: '"Muli", sans-serif',
-      textTransform: 'uppercase',
-      color: colors.textSecondary,
-    },
+      textTransform: "uppercase",
+      color: colors.textSecondary
+    }
   },
   // Color Palette
   palette: {
-    type: 'light',
+    type: "light",
     primary: {
       main: colors.primary,
       light: colors.primaryLight,
       dark: colors.primaryDark,
-      contrastText: '#FFF',
+      contrastText: "#FFF"
     },
     secondary: {
       main: colors.secondary,
       light: colors.secondaryLight,
       dark: colors.secondaryDark,
-      contrastText: '#FFF',
+      contrastText: "#FFF"
     },
     text: {
       primary: colors.textPrimary,
-      secondary: colors.textSecondary,
+      secondary: colors.textSecondary
     },
     error: {
-      main: colors.error,
+      main: colors.error
     },
     warning: {
-      main: colors.warning,
+      main: colors.warning
     },
     info: {
-      main: colors.info,
+      main: colors.info
     },
     success: {
-      main: colors.success,
-    },
+      main: colors.success
+    }
   },
   // Overriding defaults
   overrides: {
+    MuiFilledInput: {
+      root: { backgroundColor: "#f2f5f7" }
+    },
     MuiButton: {
       root: {
-        padding: '8px 20px',
+        padding: "8px 20px",
         fontFamily: '"Poppins", sans-serif',
-        textTransform: 'none',
-        fontStyle: 'normal',
-        fontSize: '20px',
-        fontWeight: 'bold',
-        letterSpacing: '0px',
-        borderRadius: '50px',
-        border: 0,
-      },
-    },
+        textTransform: "none",
+        fontStyle: "normal",
+        fontSize: "20px",
+        fontWeight: "bold",
+        letterSpacing: "0px",
+        borderRadius: "50px",
+        border: 0
+      }
+    }
   },
   props: {
     MuiButton: {
-      disableElevation: true, // Disables the box shadows
-    },
-  },
+      disableElevation: true // Disables the box shadows
+    }
+  }
 });
 export default theme;
