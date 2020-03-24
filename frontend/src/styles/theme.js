12b6a029d3fc1362a1d146bedb5506a1185cdd98
dumblole <dumblole@gmailcom>
2020-03-23 20:16:02 -0400

    textbox background changed to match figma, grid is responsive, currently WIP responsive checkbox + margins

--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -1,4 +1,4 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
 // The custom theme colors for the website
 const theme = createMuiTheme({
@@ -7,82 +7,85 @@ const theme = createMuiTheme({
     fontFamily: '"Muli", sans-serif',
     h1: {
       fontFamily: '"Poppins", sans-serif',
-      fontSize: '4rem',
-      fontWeight: 'bold',
+      fontSize: "4rem",
+      fontWeight: "bold"
     },
     h2: {
       fontFamily: '"Poppins", sans-serif',
-      fontSize: '2rem',
-      fontWeight: 'bold',
+      fontSize: "2rem",
+      fontWeight: "bold"
     },
     h3: {
       fontFamily: '"Poppins", sans-serif',
-      fontSize: '1.5rem',
-      fontWeight: 'bold',
+      fontSize: "1.5rem",
+      fontWeight: "bold"
     },
     h4: {
       fontFamily: '"Poppins", sans-serif',
-      fontSize: '1rem',
-      fontWeight: 'bold',
+      fontSize: "1rem",
+      fontWeight: "bold"
     },
     h5: {
-      fontFamily: '"Poppins", sans-serif',
+      fontFamily: '"Poppins", sans-serif'
     },
     h6: {
-      fontFamily: '"Poppins", sans-serif',
+      fontFamily: '"Poppins", sans-serif'
     },
     body1: {
-      fontSize: '1.125rem',
-    },
+      fontSize: "1.125rem"
+    }
   },
   // Color Palette
   palette: {
-    type: 'light',
+    type: "light",
     primary: {
-      main: '#006772',
-      light: '#049BAB',
-      dark: '#049BAB', // This is active tint for primary button animations
-      contrastText: '#FFF',
+      main: "#006772",
+      light: "#049BAB",
+      dark: "#049BAB", // This is active tint for primary button animations
+      contrastText: "#FFF"
     },
     secondary: {
-      main: '#320072',
-      light: '#5906C4',
-      dark: '#5906C4', // This is active tint for secondary button animations
-      contrastText: '#FFF',
+      main: "#320072",
+      light: "#5906C4",
+      dark: "#5906C4", // This is active tint for secondary button animations
+      contrastText: "#FFF"
     },
     error: {
-      main: '#F44336',
+      main: "#F44336"
     },
     warning: {
-      main: '#FF9800',
+      main: "#FF9800"
     },
     info: {
-      main: '#2196F3',
+      main: "#2196F3"
     },
     success: {
-      main: '#4CAF50',
-    },
+      main: "#4CAF50"
+    }
   },
   // Overriding defaults
   overrides: {
+    MuiFilledInput: {
+      root: { backgroundColor: "#f2f5f7" }
+    },
     MuiButton: {
       root: {
-        padding: '8px 20px',
+        padding: "8px 20px",
         fontFamily: '"Poppins", sans-serif',
-        textTransform: 'none',
-        fontStyle: 'normal',
-        fontSize: '20px',
-        fontWeight: 'bold',
-        letterSpacing: '0px',
-        borderRadius: '50px',
-        border: 0,
-      },
-    },
+        textTransform: "none",
+        fontStyle: "normal",
+        fontSize: "20px",
+        fontWeight: "bold",
+        letterSpacing: "0px",
+        borderRadius: "50px",
+        border: 0
+      }
+    }
   },
   props: {
     MuiButton: {
-      disableElevation: true, // Disables the box shadows
-    },
-  },
+      disableElevation: true // Disables the box shadows
+    }
+  }
 });
 export default theme;
