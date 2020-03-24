9dacac1a2870a6efdfba471f315dbe03323c6c35
petersteele111 <084900Pbs1!5>
2020-03-23 20:56:03 -0400

    Initial Commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -62,6 +62,6 @@ def login(web_user: schemas.Login, db: Session = Depends(get_db)):
 
 @CSM_Backend.get("/")
 async def root():
-    return {"message": "This is the root of the API. Please go to site.com/docs to see the documentation"}
+    return {"message": "This is the root of the API. Please go to site.com/docs to see the "}
 
 # Insert into the DB with something like INSERT INTO users VALUES(1, 'petersteele111@gmail.com', 'Peter', 'Steele', 'petersteele111', '012sdf02501sdf40sdf', 'True', 'True', 'Software Developer', 'Software Developer with 10 Years experience', 'Williamsburg, Michigan', '2020-03-22 12:00:00');
