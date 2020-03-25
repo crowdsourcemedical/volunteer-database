7cde18d5851439da90133fb8de51d5a366e5091c
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2020-03-25 19:30:18 +0200

    Fixes overflow issue, forces footer to the bottom of the page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,18 +21,22 @@ function App() {
     <Router>
       <div className="App">
         <NavBar />
-        <main className="content">
-          <Switch>
-            <Route path="/" exact component={HomePage} />
-            <Route path="/about" exact component={AboutPage} />
-            <Route path="/search" exact component={SearchPage} />
-            <Route path="/signup" exact component={SignupPage} />
-            <Route path="/signup/volunteer" exact component={SignupVolunteerPage} />
-            <Route path="/projects/submit" exact component={ProjectSubmit} />
-            <Route path="/profile" exact component={Profile} />
-            <Route path="/style" exact component={Style} />
-            <Route path="/project" exact component={Project} />
-          </Switch>
+        <main className="main">
+          <div className="content">
+            <Switch>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/about" exact component={AboutPage} />
+              <Route path="/search" exact component={SearchPage} />
+              <Route path="/signup" exact component={SignupPage} />
+              <Route path="/signup/volunteer" exact component={SignupVolunteerPage} />
+              <Route path="/projects/submit" exact component={ProjectSubmit} />
+              <Route path="/profile" exact component={Profile} />
+              <Route path="/style" exact component={Style} />
+              <Route path="/project" exact component={Project} />
+            </Switch>
+          </div>
+
+          <Footer />
         </main>
       </div>
     </Router>
