92ed688359011e748ba2376837eada9c24ef24c8
Jon Ludwig <jludwig2552@gmail.com>
2020-03-26 14:04:51 -0400

    Switched CSM with CSS for everything backend
    Added script for running pytest from windows w/o make

---
+++ backend/scripts/pytest.bat
@@ -0,0 +1,10 @@
+CALL :NORMALIZEPATH "%~dp0..\"
+SET "BACKEND_DIR=%RETVAL%"
+
+docker run -ti --rm -v %BACKEND_DIR%:/app -e ENV_NAME=test css_backend pytest -v -s
+
+:: ========== FUNCTIONS =============
+EXIT /B 
+:NORMALIZEPATH
+    SET RETVAL=%~dpfn1
+    EXIT /B
